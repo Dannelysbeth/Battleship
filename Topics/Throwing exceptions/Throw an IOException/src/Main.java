763a5e13f660c8c7d509e4bb0d9e3bbe7b76e31f
danifoj315 <danifoj315@student.polsl.pl>
2023-06-21 00:07:40 +0200

    completed board

--- Topics/Throwing exceptions/Throw an IOException/src/Main.java
+++ Topics/Throwing exceptions/Throw an IOException/src/Main.java
@@ -7,13 +7,13 @@ public class Main {
     // change this method
     public static void method() {
         char[][] board = new char[BOARD_SIZE ][BOARD_SIZE];
-        board[0][0] = ' ';
-//        for (int i = 1; i <= BOARD_SIZE; i++) {
-//            i > 9 ? board[0][i] = (char) ('0' + i) : board[0][i] = ;
-//            board[i][0] = (char) ('A' + i - 1);
-//        }
-        for (int i = 1; i <= BOARD_SIZE; i++) {
-            for (int j = 1; j <= BOARD_SIZE; j++) {
+//        board[0][0] = ' ';
+////        for (int i = 1; i <= BOARD_SIZE; i++) {
+////            i > 9 ? board[0][i] = (char) ('0' + i) : board[0][i] = ;
+////            board[i][0] = (char) ('A' + i - 1);
+////        }
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
                 board[i][j] = '~';
             }
         }
@@ -22,7 +22,7 @@ public class Main {
             System.out.print(" " + j);
         }
         System.out.println();
-        for (int i = 0; i <= BOARD_SIZE; i++) {
+        for (int i = 0; i < BOARD_SIZE; i++) {
 
             char character = (char) ('A' + i);
             System.out.print(character + " ");
